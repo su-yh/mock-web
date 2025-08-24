e9641c3e0cfedba08fa92c2765d5ebe0c729aabd
suyh <su787910081@163.com>
2025-08-24 22:29:09 +0800

    v2 版本

--- src/api/mock/config/datasource/index.ts
+++ src/api/mock/config/datasource/index.ts
@@ -1,9 +1,10 @@
 import request from '@/utils/request'
 import {ResponseBase, PageParams, PageResult} from "@/api/base/types";
-import {EnvDatasourcePropertiesEntity} from './types'
+import {DataSourceEnums, EnvDatasourcePropertiesEntity} from './types'
 
 enum API {
   LIST_PAGE_URL = '/env/datasource/listPage',
+  QUERY_DATASOURCE_BY_ENV_URL = '/env/datasource/queryDataSourceByEnv',
   CREATE_URL = '/env/datasource/create',
   UPDATE_URL = '/env/datasource/update',
   DELETE_URL = '/env/datasource/delete',
@@ -11,6 +12,7 @@ enum API {
 }
 
 export const listPageReq = (pageParams: PageParams) => request.get<PageParams, ResponseBase<PageResult<EnvDatasourcePropertiesEntity>>>(API.LIST_PAGE_URL, {params: pageParams})
+export const queryDataSourceByEnvReq = (env: string, dataSourceName: DataSourceEnums) => request.get<any, ResponseBase<EnvDatasourcePropertiesEntity>>(API.QUERY_DATASOURCE_BY_ENV_URL, {params: {env, dataSourceName}})
 export const createReq = (entity: EnvDatasourcePropertiesEntity) => request.post<EnvDatasourcePropertiesEntity, ResponseBase>(API.CREATE_URL, entity)
 export const updateReq = (entity: EnvDatasourcePropertiesEntity) => request.post<EnvDatasourcePropertiesEntity, ResponseBase>(API.UPDATE_URL, entity);
 export const deleteReq = (id: string) => request.post<string, ResponseBase>(API.DELETE_URL, {id})
--- src/views/mock/v2/attr/index.vue
+++ src/views/mock/v2/attr/index.vue
@@ -20,8 +20,9 @@
   <!-- 只有选定了某个环境才会出现配置标签页 -->
   <el-card v-if="envEntity.env">
     <el-tabs v-if="envEntity.env" style="margin: 10px 0" type="border-card" v-model="tabData.activeTabName" @tab-change="handleTabChange">
+
+      <!-- 平台属性配置 -->
       <el-tab-pane :label="TabNameEnum.Platform" :name="TabNameEnum.Platform">
-        <div style="margin: 10px 0"><span>{{envEntity.env}} 环境平台相关的属性配置： </span></div>
         <el-card shadow="always">
           <el-form label-width="160px">
             <el-form-item label="mode: ">
@@ -53,14 +54,96 @@
             <el-form-item label="每次最大提现数量">
               <el-input-number v-model="tabData.platformEntity.randomMaxWithdrawal" placeholder="每次最大提现数量" :controls="false"/>
             </el-form-item>
+
+            <!-- TODO: suyh - 保存，更新或者创建 -->
             <el-button type="primary" size="large">保存</el-button>
           </el-form>
         </el-card>
       </el-tab-pane>
+
+      <!-- 数据源配置 -->
       <el-tab-pane :label="TabNameEnum.DataSource" :name="TabNameEnum.DataSource">
-        <!-- TODO: suyh - 这里配置数据源 -->
-        这里配置数据源
+        <!-- 数据源：flinkCds -->
+        <el-card>
+          <el-form label-width="120px">
+            <el-form-item label="dataSourceName: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkCds.dataSourceName" disabled/>
+              </template>
+            </el-form-item>
+            <el-form-item label="jdbcUrl: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkCds.jdbcUrl" placeholder="请输入jdbcUrl"/>
+              </template>
+            </el-form-item>
+            <el-form-item label="username: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkCds.username" placeholder="请输入username"/>
+              </template>
+            </el-form-item>
+            <el-form-item label="password: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkCds.password" placeholder="请输入password"/>
+              </template>
+            </el-form-item>
+            <el-form-item label="driverClassName: ">
+              <template #default>
+                <el-select v-model="tabData.dataSources.flinkCds.driverClassName">
+                  <el-option :value="'com.mysql.cj.jdbc.Driver'" :label="'mysql(com.mysql.cj.jdbc.Driver)'"/>
+                  <el-option :value="'org.postgresql.Driver'" :label="'pgsql(org.postgresql.Driver)'"/>
+                </el-select>
+              </template>
+            </el-form-item>
+            <el-form-item label="flywayEnabled: ">
+              <template #default>
+                <el-switch v-model="tabData.dataSources.flinkCds.flywayEnabled"/>
+              </template>
+            </el-form-item>
+            <el-button type="primary">保存</el-button>
+          </el-form>
+        </el-card>
+
+        <!-- 数据源：flinkPgCdap -->
+        <el-card style="margin-top: 10px">
+          <el-form label-width="120px">
+            <el-form-item label="dataSourceName: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkPgCdap.dataSourceName" disabled/>
+              </template>
+            </el-form-item>
+            <el-form-item label="jdbcUrl: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkPgCdap.jdbcUrl" placeholder="请输入jdbcUrl"/>
+              </template>
+            </el-form-item>
+            <el-form-item label="username: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkPgCdap.username" placeholder="请输入username"/>
+              </template>
+            </el-form-item>
+            <el-form-item label="password: ">
+              <template #default>
+                <el-input v-model="tabData.dataSources.flinkPgCdap.password" placeholder="请输入password"/>
+              </template>
+            </el-form-item>
+            <el-form-item label="driverClassName: ">
+              <template #default>
+                <el-select v-model="tabData.dataSources.flinkPgCdap.driverClassName">
+                  <el-option :value="'com.mysql.cj.jdbc.Driver'" :label="'mysql(com.mysql.cj.jdbc.Driver)'"/>
+                  <el-option :value="'org.postgresql.Driver'" :label="'pgsql(org.postgresql.Driver)'"/>
+                </el-select>
+              </template>
+            </el-form-item>
+            <el-form-item label="flywayEnabled: ">
+              <template #default>
+                <el-switch v-model="tabData.dataSources.flinkPgCdap.flywayEnabled"/>
+              </template>
+            </el-form-item>
+            <el-button type="primary">保存</el-button>
+          </el-form>
+        </el-card>
       </el-tab-pane>
+
       <el-tab-pane :label="TabNameEnum.RabbitMq" :name="TabNameEnum.RabbitMq">
         <!-- TODO: suyh - 这里配置RabbitMQ -->
         这里配置RabbitMQ
@@ -78,6 +161,8 @@ import {ResponseBase} from "@/api/base/types";
 import {ElMessage} from "element-plus";
 import {MockModeEnums, MockPropertiesEntity} from "@/api/mock/config/platform/types";
 import {queryPlatformByEnvReq} from "@/api/mock/config/platform";
+import {queryDataSourceByEnvReq} from '@/api/mock/config/datasource'
+import {DataSourceEnums, EnvDatasourcePropertiesEntity} from "@/api/mock/config/datasource/types";
 
 enum TabNameEnum {
   Platform = 'MockConfigPlatform',
@@ -88,7 +173,11 @@ enum TabNameEnum {
 interface TabData {
   activeTabName: TabNameEnum, // 当前选中的标签页
   platformEntity: MockPropertiesEntity,  // platform 标签数据
-  dataSourceEntity: any, // datasource 标签数据
+  // datasource 标签数据
+  dataSources: {
+    flinkCds: EnvDatasourcePropertiesEntity,
+    flinkPgCdap: EnvDatasourcePropertiesEntity
+  },
   rabbitMqEntity: any,  // rabbitmq 标签数据
 }
 
@@ -97,7 +186,14 @@ const envList = ref<MockEnvConfigEntity[]>([])
 // 选中的env 环境数据
 const envEntity = ref<MockEnvConfigEntity>({env: '', enabled: false})
 // tabs 标签页相关的数据属性对象
-let tabData = reactive<TabData>({activeTabName: TabNameEnum.Platform, platformEntity: {}})
+let tabData = reactive<TabData>({
+  activeTabName: TabNameEnum.Platform,
+  platformEntity: {},
+  dataSources: {
+    flinkCds: {},
+    flinkPgCdap: {},
+  }
+})
 
 onMounted(async () => {
   const result: ResponseBase<MockEnvConfigEntity[]> = await listAllReq();
@@ -121,7 +217,22 @@ const handleTabChange = async (tabName: TabNameEnum) => {
       tabData.platformEntity = result.data ? result.data : {}
       break
     case TabNameEnum.DataSource:
-      console.log('处理数据源配置逻辑')
+    {
+      const result: ResponseBase<EnvDatasourcePropertiesEntity> = await queryDataSourceByEnvReq(envEntity.value.env, DataSourceEnums.FLINK_CDS);
+      if (result.code != 0) {
+        ElMessage({type: 'error', message: result.message})
+        return
+      }
+      tabData.dataSources.flinkCds = result.data ? result.data : {dataSourceName: DataSourceEnums.FLINK_CDS}
+    }
+    {
+      const result: ResponseBase<EnvDatasourcePropertiesEntity> = await queryDataSourceByEnvReq(envEntity.value.env, DataSourceEnums.FLINK_PG_CDAP);
+      if (result.code != 0) {
+        ElMessage({type: 'error', message: result.message})
+        return
+      }
+      tabData.dataSources.flinkPgCdap = result.data ? result.data : {dataSourceName: DataSourceEnums.FLINK_PG_CDAP}
+    }
       break
     case TabNameEnum.RabbitMq:
       console.log('处理RabbitMQ配置逻辑')
