d2d35c5db86876724ceecf41358038fb63664cf5
suyh <su787910081@163.com>
2025-08-24 17:54:37 +0800

    v2 版本

--- src/views/mock/v2/attr/index.vue
+++ src/views/mock/v2/attr/index.vue
@@ -5,7 +5,7 @@
 
   <el-card class="envCard">
     <div class="envSelect">
-      <span>选择对应的环境： </span>
+      <span>环境： </span>
       <el-select style="width: 200px" v-model="envEntity" placeholder="请选择" size="large">
         <template #label>
           <span v-if="envEntity.env">{{envEntity.env}}(<span :style="{ color: envEntity.enabled ? 'blue' : 'red' }">{{envEntity.enabled ? '启用' : '禁用'}}</span>)</span>
@@ -17,14 +17,54 @@
     </div>
   </el-card>
 
-  <el-card>
-    <!-- 使用多标签页 -->
-    <el-tabs>
+  <!-- 只有选定了某个环境才会出现配置标签页 -->
+  <el-card v-if="envEntity.env">
+    <el-tabs v-if="envEntity.env" style="margin: 10px 0" type="border-card">
       <el-tab-pane label="MockConfigPlatform">
-        <span>{{envEntity.env}} 环境平台相关的属性配置： </span>
+        <div style="margin: 10px 0"><span>{{envEntity.env}} 环境平台相关的属性配置： </span></div>
+        <el-card shadow="always">
+          <el-form label-width="160px">
+            <el-form-item label="mode: ">
+              <el-select v-model="platformEntity.mode" placeholder="请选择mode">
+                <el-option :value="MockModeEnums.NONE" label="NONE" />
+                <el-option :value="MockModeEnums.TIMER_JOB" label="TIMER_JOB" />
+                <el-option :value="MockModeEnums.DATE_RANGE" label="DATE_RANGE" />
+                <el-option :value="MockModeEnums.TS_RANGE" label="TS_RANGE" />
+              </el-select>
+            </el-form-item>
+            <el-form-item label="开始时间：">
+              <el-date-picker v-model="platformEntity.tsBegin" type="datetime" placeholder="开始时间" :default-time="['00:00:00']" value-format="x"/>
+            </el-form-item>
+            <el-form-item label="结束时间(不包含)：">
+              <el-date-picker v-model="platformEntity.tsEnd" type="datetime" placeholder="结束时间" :default-time="['00:00:00']" value-format="x"/>
+            </el-form-item>
+            <el-form-item label="子渠道数量">
+              <el-input-number v-model="platformEntity.subChannelCount" placeholder="子渠道数量" :controls="false"/>
+            </el-form-item>
+            <el-form-item label="每次最大注册数量">
+              <el-input-number v-model="platformEntity.randomMaxRegistry" placeholder="每次最大注册数量" :controls="false"/>
+            </el-form-item>
+            <el-form-item label="每次最大登录数量">
+              <el-input-number v-model="platformEntity.randomMaxLogin" placeholder="每次最大登录数量" :controls="false"/>
+            </el-form-item>
+            <el-form-item label="每次最大充值数量">
+              <el-input-number v-model="platformEntity.randomMaxRecharge" placeholder="每次最大充值数量" :controls="false"/>
+            </el-form-item>
+            <el-form-item label="每次最大提现数量">
+              <el-input-number v-model="platformEntity.randomMaxWithdrawal" placeholder="每次最大提现数量" :controls="false"/>
+            </el-form-item>
+            <el-button type="primary" size="large">保存</el-button>
+          </el-form>
+        </el-card>
       </el-tab-pane>
-      <el-tab-pane label="MockConfigDataSource">Config</el-tab-pane>
-      <el-tab-pane label="MockConfigRabbitMq">Role</el-tab-pane>
+      <el-tab-pane label="MockConfigDataSource">
+        <!-- TODO: suyh - 这里配置数据源 -->
+        这里配置数据源
+      </el-tab-pane>
+      <el-tab-pane label="MockConfigRabbitMq">
+        <!-- TODO: suyh - 这里配置RabbitMQ -->
+        这里配置RabbitMQ
+      </el-tab-pane>
     </el-tabs>
   </el-card>
 </template>
@@ -36,11 +76,13 @@ import {MockEnvConfigEntity} from "@/api/mock/config/env/types";
 import {listAllReq} from "@/api/mock/config/env";
 import {ResponseBase} from "@/api/base/types";
 import {ElMessage} from "element-plus";
+import {MockModeEnums, MockPropertiesEntity} from "@/api/mock/config/platform/types";
 
 // 所有的env 环境列表数据
 const envList = ref<MockEnvConfigEntity[]>([])
 // 选中的env 环境数据
 const envEntity = ref<MockEnvConfigEntity>({env: '', enabled: false})
+let platformEntity = ref<MockPropertiesEntity>({});
 
 onMounted(async () => {
   const result: ResponseBase<MockEnvConfigEntity[]> = await listAllReq();
@@ -62,11 +104,6 @@ onMounted(async () => {
   .envSelect {
     display: flex;
     align-items: center;
-
-    span {
-      width: 160px;
-    }
-
   }
 }
 
